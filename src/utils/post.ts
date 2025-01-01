83d3bbc9ba682faa0cab461308ae966b3fa8507e
Matt Batman <batman.matt@gmail.com>
2025-01-01 17:15:20 -0500

    Sort Bible chapters in directory

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,6 +1,75 @@
 import type { CollectionEntry } from "astro:content";
 import { getCollection } from "astro:content";
 
+const BIBLE_BOOKS = [
+	"Genesis",
+	"Exodus",
+	"Leviticus",
+	"Numbers",
+	"Deuteronomy",
+	"Joshua",
+	"Judges",
+	"Ruth",
+	"1 Samuel",
+	"2 Samuel",
+	"1 Kings",
+	"2 Kings",
+	"1 Chronicles",
+	"2 Chronicles",
+	"Ezra",
+	"Nehemiah",
+	"Esther",
+	"Job",
+	"Psalm",
+	"Proverbs",
+	"Ecclesiastes",
+	"Song of Solomon",
+	"Isaiah",
+	"Jeremiah",
+	"Lamentations",
+	"Ezekiel",
+	"Daniel",
+	"Hosea",
+	"Joel",
+	"Amos",
+	"Obadiah",
+	"Jonah",
+	"Micah",
+	"Nahum",
+	"Habakkuk",
+	"Zephaniah",
+	"Haggai",
+	"Zechariah",
+	"Malachi",
+	"Matthew",
+	"Mark",
+	"Luke",
+	"John",
+	"Acts",
+	"Romans",
+	"1 Corinthians",
+	"2 Corinthians",
+	"Galatians",
+	"Ephesians",
+	"Philippians",
+	"Colossians",
+	"1 Thessalonians",
+	"2 Thessalonians",
+	"1 Timothy",
+	"2 Timothy",
+	"Titus",
+	"Philemon",
+	"Hebrews",
+	"James",
+	"1 Peter",
+	"2 Peter",
+	"1 John",
+	"2 John",
+	"3 John",
+	"Jude",
+	"Revelation",
+];
+
 /** Note: this function filters out draft posts based on the environment */
 export async function getAllPosts() {
 	return await getCollection("post", ({ data }) => {
@@ -15,6 +84,15 @@ export async function getAllBibleChapters() {
 		const { pageType } = data;
 
 		return pageType === "bibleChapter";
+	});
+}
+
+export function sortBibleChapterPosts(posts: Array<CollectionEntry<"post">>) {
+	// sort the list of posts by title in order of BIBLE_BOOKS
+	return posts.sort((a, b) => {
+		const aIndex = BIBLE_BOOKS.indexOf(a.data.title);
+		const bIndex = BIBLE_BOOKS.indexOf(b.data.title);
+		return aIndex - bIndex;
 	});
 }
 
